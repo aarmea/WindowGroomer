b347e74f489ad33f5c3b46c38e7c648bcd823e29
Albert Armea <alvin.armea@gmail.com>
2012-12-28 03:52:55 -0500

    GridWidget draws a grid
    Added a paintCells method that draws cells defined by a QRect -
    each pixel in the QRect corresponds to a cell that is drawn.
    
    The GridWidget::activeCells is intended to store the cells that
    have been dragged over, but this has not been implemented yet.

--- gridwidget.cpp
+++ gridwidget.cpp
@@ -1,11 +1,51 @@
 #include "gridwidget.h"
 
 GridWidget::GridWidget(QWidget *parent) :
-  QWidget(parent), cols(4), rows(3)
+  QWidget(parent),
+  cols(4), rows(3),
+  backColor(Qt::black),
+  inactiveCellColor(Qt::darkGray),
+  activeCellColor(Qt::lightGray)
 {}
 
 void GridWidget::resizeGrid(int newCols, int newRows)
 {
   cols = newCols;
   rows = newRows;
+}
+
+void GridWidget::setColors(QColor back, QColor inactive, QColor active)
+{
+  backColor = back;
+  inactiveCellColor = inactive;
+  activeCellColor = active;
+}
+
+void GridWidget::paintEvent(QPaintEvent *event)
+{
+  QPainter painter(this);
+
+  // Fill with background
+  painter.fillRect(rect(), backColor);
+
+  // Draw inactive cells
+  paintCells(painter, QRect(0, 0, cols, rows), Inactive);
+
+  // Draw active cells
+  paintCells(painter, activeCells, Active);
+}
+
+void GridWidget::paintCells(QPainter &painter, QRect cells, CellStatus status)
+{
+  int border = 2;
+  QColor color = (status == Active) ? activeCellColor : inactiveCellColor;
+  int cellWidth = width() / cols;
+  int cellHeight = height() / rows;
+  for (int x = cells.left(); x <= cells.right(); ++x) {
+    for (int y = cells.top(); y <= cells.bottom(); ++y) {
+      QRect cell(x*cellWidth + border, y*cellHeight + border,
+        cellWidth - 2*border, cellHeight - 2*border);
+      painter.fillRect(cell, color);
+    }
+  }
 }
--- gridwidget.h
+++ gridwidget.h
@@ -2,8 +2,10 @@
 #define GRIDWIDGET_H
 
 #include <QWidget>
-
-#include <QApplication>
+#include <QPaintEvent>
+#include <QPainter>
+#include <QRect>
+#include <QColor>
 
 class GridWidget : public QWidget
 {
@@ -12,10 +14,24 @@ public:
   explicit GridWidget(QWidget *parent = 0);
 
   void resizeGrid(int newCols, int newRows);
+  void setColors(QColor back, QColor inactive, QColor active);
+
+protected:
+  enum CellStatus {Inactive, Active};
+
+  void paintEvent(QPaintEvent *event);
+  void paintCells(QPainter &painter, QRect cells,
+    CellStatus status = Inactive);
 
 private:
   int cols;
   int rows;
+  int border;
+  QColor backColor;
+  QColor inactiveCellColor;
+  QColor activeCellColor;
+
+  QRect activeCells;
 };
 
 #endif
